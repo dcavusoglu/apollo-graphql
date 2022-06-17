363a4898867502ea65b2520984ae5b209dcb7579
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-06-17 13:59:35 +0300

    schema created

---
+++ odyssey-lift-off-part1/server/src/schema.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+
+type Query {
+  "Get tracks array for homepage grid"
+  tracksForHome: [Track!]!
+}
+
+"A track is a group of mudules teaching about a specific topic"
+
+type Track {
+  id: ID!
+  title: String!
+  author: Author!
+  thumbnail: String
+  length: Int
+  moduleCount: Int
+}
+
+"Author of a track or a module"
+type Author {
+  id: ID!
+  name: String!
+  photo: String
+}
+`;
+
+module.exports = typeDefs;
