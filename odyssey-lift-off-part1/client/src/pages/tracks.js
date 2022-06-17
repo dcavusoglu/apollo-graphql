20d4a35416389ec003873cb560f90fc0a7014b2b
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-06-17 17:22:25 +0300

    data retrieved

--- odyssey-lift-off-part1/client/src/pages/tracks.js
+++ odyssey-lift-off-part1/client/src/pages/tracks.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { Layout } from '../components';
+import {gql, useQuery} from '@apollo/client';
+import TrackCard from '../containers/track-card';
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+
+export const TRACKS =gql`
+query TracksForHome {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      photo
+      name
+      id
+    }
+  }
+}
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const {loading, error, data} = useQuery(TRACKS);
+  if(loading) return 'Loading...';
+  if(error) return `Error! ${error.message}`;
+  return <Layout grid>
+    {data?.tracksForHome?.map(track => (
+      <TrackCard key={track.id} track={track} />
+    ))}
+    </Layout>;
 };
 
 export default Tracks;
